081ea415e878854dbe5e58da48c5654105db8126
Victor Jp <victorjpantaleao2gmail.com>
2022-06-28 10:01:26 -0500

    versão 1.0

---
+++ exemplo02/exemplo2.pas
@@ -0,0 +1,31 @@
+unit exemplo2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BtCalcular: TButton;
+    bbSair: TButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
